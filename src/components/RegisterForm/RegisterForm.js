2c7bd9c1948d5691254bcd6073884f433cad8ec3
Malakhow-Alexandr <chuk3pc@gmail.com>
2023-04-19 10:58:24 +0300

    add register validation

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
 import {
@@ -12,6 +13,8 @@ import {
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -24,6 +27,19 @@ export const RegisterForm = () => {
       })
     );
     form.reset();
+  };
+
+  const handlePasswordChange = event => {
+    const value = event.target.value;
+    setPassword(value);
+
+    // Валидация на минимальное количество символов и наличие цифр и букв
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
+    if (!passwordRegex.test(value)) {
+      setError(true);
+    } else {
+      setError(false);
+    }
   };
 
   return (
@@ -64,6 +80,14 @@ export const RegisterForm = () => {
           label="Password"
           variant="outlined"
           size="small"
+          value={password}
+          onChange={handlePasswordChange}
+          error={error}
+          helperText={
+            error &&
+            'Пароль должен содержать минимум 7 символов и включать цифры и буквы'
+          }
+          inputProps={{ minLength: 7 }}
         />
       </InputWrapper>
 

345f297da8860d332614f3e6523de5d58591e1be
Malakhow-Alexandr <chuk3pc@gmail.com>
2023-04-19 09:26:08 +0300

    add weather widget animation

--- src/components/Weather/WeatherApp.js
+++ src/components/Weather/WeatherApp.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import styles from './WeatherApp.module.css';
+import { motion, AnimatePresence } from 'framer-motion';
 
-export const Weather = () => {
+export const Weather = ({ onMount }) => {
   const [query, setQuery] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
   const [weather, setWeather] = useState({
     data: {},
     error: false,
@@ -66,52 +68,71 @@ export const Weather = () => {
 
   return (
     <div className={styles.forecast}>
-      <div className={styles.wrapper}>
-        <div className={styles.wrapper_search}>
-          <h2 className={styles.date}>{toDate()}</h2>
-          <input
-            placeholder="City"
-            className={styles.input_field}
-            autoFocus
-            type="text"
-            name="query"
-            value={query}
-            onChange={event => setQuery(event.target.value)}
-            onKeyPress={search}
-          />
-        </div>
-      </div>
+      <AnimatePresence>
+        <motion.div
+          className={styles.wrapper}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.5 }}
+          transition={{ duration: 0.4 }}
+        >
+          <div className={styles.wrapper_search}>
+            <h2 className={styles.date}>{toDate()}</h2>
+            <input
+              placeholder="City"
+              className={styles.input_field}
+              autoFocus
+              type="text"
+              name="query"
+              value={query}
+              onChange={event => setQuery(event.target.value)}
+              onKeyPress={search}
+            />
+          </div>
+        </motion.div>
+      </AnimatePresence>
       {weather.error ||
         (weather && weather.data && weather.data.main && (
-          <div className={styles.wrapper__bg}>
-            {weather.error && (
-              <span style={{ fontSize: '26px' }}> Sorry, City not found</span>
-            )}
+          <AnimatePresence>
+            <motion.div
+              className={styles.wrapper__bg}
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5 }}
+              transition={{
+                duration: 0.3,
+                type: 'just',
+              }}
+            >
+              {weather.error && (
+                <span style={{ fontSize: '26px' }}> Sorry, City not found</span>
+              )}
 
-            {weather && weather.data && weather.data.main && (
-              <div>
-                <h2 className={styles.city}>
-                  {weather.data.name}, <span>{weather.data.sys.country}</span>{' '}
-                </h2>
-                <div className={styles.wrap_icon}>
-                  <img
-                    className={styles.weather_icon}
-                    src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
-                    alt={weather.data.weather[0].description}
-                  />
+              {weather && weather.data && weather.data.main && (
+                <div>
+                  <h2 className={styles.city}>
+                    {weather.data.name}, <span>{weather.data.sys.country}</span>{' '}
+                  </h2>
+                  <div className={styles.wrap_icon}>
+                    <img
+                      className={styles.weather_icon}
+                      src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
+                      alt={weather.data.weather[0].description}
+                    />
 
-                  <p className={styles.temp}>
-                    {Math.round(weather.data.main.temp)}
-                    <span> &deg;C </span>
+                    <p className={styles.temp}>
+                      {Math.round(weather.data.main.temp)}
+                      <span> &deg;C </span>
+                    </p>
+                  </div>
+
+                  <p className={styles.color}>
+                    Wind Speed: {weather.data.wind.speed}m/s
                   </p>
                 </div>
-
-                <p className={styles.color}>
-                  Wind Speed: {weather.data.wind.speed}m/s
-                </p>
-              </div>
-            )}
-          </div>
+              )}
+            </motion.div>
+          </AnimatePresence>
         ))}
     </div>
   );
--- src/components/WeatherButton/WeatherButton.styled.jsx
+++ src/components/WeatherButton/WeatherButton.styled.jsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const WeatherButton = styled.button`
   position: absolute;
-  top: -55px;
+  top: -58px;
   left: 350px;
   padding: 0;
   border: none;
